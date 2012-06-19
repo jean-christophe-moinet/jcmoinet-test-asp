57119a4caf68bf951f8ec7ee59b3a59af07b8bf9
Jean-Christophe Moinet <jean-christophe.moinet@valtech.fr>
2012-06-19 09:03:33 +0200

    First import

---
+++ TestApplication/TestApplication.Lib/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TestApplication.Lib
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string SayHello(string name)
+        {
+            return string.Format("Hello, {0}", name);
+        }
+    }
+}
