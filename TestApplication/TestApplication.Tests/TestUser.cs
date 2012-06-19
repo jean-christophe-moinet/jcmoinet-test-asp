2a3290ff4347237e8b7872da3d0f5ffd973413ea
Jean-Christophe Moinet <jean-christophe.moinet@valtech.fr>
2012-06-19 09:03:51 +0200

    First commit

---
+++ TestApplication/TestApplication.Tests/TestUser.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TestApplication.Lib;
+
+namespace TestApplication.Tests
+{
+    [TestFixture]
+    public class TestUser
+    {
+        [Test]
+        public void TestSayHello()
+        {
+            var user = new User(); 
+            Assert.AreEqual("Hello, JCM", user.SayHello("JCM"));
+        }
+    }
+}
